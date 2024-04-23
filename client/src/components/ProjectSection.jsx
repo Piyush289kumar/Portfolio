cc031e9cb93b792d36af0c60fd4797c8ff3fa7ed
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-23 20:52:52 +0530

    useRef used in Project Section for Scrolling Card

--- client/src/components/ProjectSection.jsx
+++ client/src/components/ProjectSection.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import ProjectCard from "./ProjectCard";
 import { HiArrowSmLeft } from "react-icons/hi";
 import { HiArrowSmRight } from "react-icons/hi";
-
+import { useRef } from "react";
 function ProjectSection() {
+  const scrollRef = useRef(null);
+  const scrollRight = () => {
+    scrollRef.current.scrollLeft += 500;
+  };
+  const scrollLeft = () => {
+    scrollRef.current.scrollLeft -= 500;
+  };
   return (
     <div className="mb-20 lg:mb-36">
       <h1 className="text-5xl lg:text-7xl gradientText mb-10">
         My Recent Project
       </h1>
-      <div className="flex overflow-x-scroll gap-8 lg:p-3 scrollHide">
+      <div
+        ref={scrollRef}
+        className="flex overflow-x-scroll gap-8 lg:p-3 scrollHide"
+      >
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
@@ -21,14 +31,19 @@ function ProjectSection() {
         <ProjectCard />
       </div>
       <div className="flex justify-center items-center gap-3 mt-2 select-none">
-        <HiArrowSmLeft className="text-xl lg:text-2xl cursor-pointer text-yellow-500 hidden lg:block" />
+        <HiArrowSmLeft
+          className="text-xl lg:text-2xl cursor-pointer text-yellow-500 hidden lg:block"
+          onClick={scrollLeft}
+        />
         <h2 className="font-mono uppercase gradientText text-lg">
           Slide for More
         </h2>
-        <HiArrowSmRight className="text-xl lg:text-2xl cursor-pointer text-yellow-500 hidden lg:block" />
+        <HiArrowSmRight
+          className="text-xl lg:text-2xl cursor-pointer text-yellow-500 hidden lg:block"
+          onClick={scrollRight}
+        />
       </div>
     </div>
   );
 }
-
 export default ProjectSection;
