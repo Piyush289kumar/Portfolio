f6f1efbb27ba60d6490128c6c11e780416e84e98
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-23 17:41:08 +0530

    Skill Section Is Builed

--- client/src/components/SkillCard.jsx
+++ client/src/components/SkillCard.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-function SkillCard() {
-  let width = 20;
+function SkillCard({ technology, value }) {
   return (
     <div className="flex justify-between items-center mt-5 backdrop-blur-xl shadow-xl p-3 border-2 rounded-lg border-gray-400 mr-2">
-      <h3 className="font-bold text-xl text-white">React Js</h3>
+      <h3 className="font-bold text-xl text-white">{technology}</h3>
       <div className="progressBar w-[25%] h-[25px]">
-        <div className={`progressBarVal h-full w-[${width}%]`}></div>
+        <div className={`progressBarVal h-full w-[${value}%]`}></div>
       </div>
     </div>
   );
--- client/src/components/SkillSection.jsx
+++ client/src/components/SkillSection.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import SkillCard from "./SkillCard";
 
+const skillData = [
+  { technology: "PHP", value: "40" },
+  { technology: "MySQL", value: "25" },
+  { technology: "React Js", value: "20" },
+  { technology: "Node Js", value: "35" },
+  { technology: "Express Js", value: "25" },
+  { technology: "MongoDB", value: "20" },
+  { technology: "Corel Draw", value: "35" },
+];
+
 function SkillSection() {
   return (
-    <div className="">
-      <SkillCard />
+    <div className="mb-20 lg:mb-36">
+      <h1 className="text-5xl lg:text-7xl gradientText mb-10">My Skills</h1>
+      <div className="h-[300px] overflow-y-scroll select-none scrollBar cursor-pointer">
+        {skillData.map((skill, idx) => (
+          <SkillCard
+            key={idx}
+            technology={skill.technology}
+            value={skill.value}
+          />
+        ))}
+      </div>
     </div>
   );
 }
