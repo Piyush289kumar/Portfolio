fd4a704b12317f163f4eefd5125b853768953023
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-24 07:49:04 +0530

    Adding AOS For Animation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,12 @@ import {
   ProjectSection,
   ContactSection,
 } from "../components/index.js";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
+AOS.init({
+  duration: 1000,
+});
 function Home() {
   return (
     <div className="w-[80vw] mx-auto">
