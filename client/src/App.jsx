6aa5d5386f6850043e18fbed69f9b80252ceacea
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-30 20:46:15 +0530

    React Router Dom init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import Home from "./pages/Home.jsx";
+import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
