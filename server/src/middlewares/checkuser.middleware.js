b676d152d10e50b9e1f9f2851e28a3d708d7a0b4
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-30 07:24:11 +0530

    "Remove unnecessary variables and simplify code in checkUser middleware"
    This commit removes some unnecessary variables and simplifies the code in the `checkUser` middleware function. The `refreshToken` is now destructured directly from the

--- server/src/middlewares/checkuser.middleware.js
+++ server/src/middlewares/checkuser.middleware.js
@@ -1,40 +1,32 @@
 import jwt from "jsonwebtoken";
 import { ApiError, ApiResponse, asycHandler } from "../utils";
-
 const checkUser = asycHandler(async (req, res, next) => {
 	try {
 		const { refreshToken } =
 			req.cookies?.refreshToken || req.body?.refreshToken;
-
 		if (!refreshToken) {
 			return res
 				.status(401)
 				.json(new ApiResponse(401, false, "Please Login First"));
 		}
-
 		const verifyRefreshToken = jwt.verify(
 			refreshToken,
 			process.env.REFRESH_TOKEN_SECRET
 		);
-
 		if (!verifyRefreshToken) {
 			throw new ApiError(401, false, "Unable to Verify Refresh Token");
 		}
-
 		res.clearCookie("refreshToken");
-
 		const newRefreshToken = jwt.sign(
 			{ id: user.id },
 			process.env.REFRESH_TOKEN_SECRET
 		);
-
 		res.cookies("refreshToken", newRefreshToken, {
 			path: "/",
 			httpOnly: true,
 			expiresIn: new Date(Date.now() + 1000 * 30),
 			sameSite: "lax",
 		});
-
 		return res
 			.status(200)
 			.json(new ApiResponse(200, true, user, "User is logged in"));
