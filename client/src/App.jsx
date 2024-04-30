29364a0bd14666ffcca920b5b3a8a8e7af48d521
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-30 20:54:59 +0530

    Create Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import Home from "./pages/Home.jsx";
 import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AdminLogin from "./pages/Admin/AdminLogin.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/login" element={<AdminLogin />} />
       </Routes>
     </BrowserRouter>
   );
