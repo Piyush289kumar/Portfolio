813b1570dc3ee3fbc1eb9b67711f2a69c9b2846d
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-30 18:17:42 +0530

    "Use nodemailer to send contact message; update error and success responses"
    This commit updates the contact controller to send an email using nodemailer instead of simply creating a user message string. It also updates the error and success responses to

--- server/src/controllers/contact.controller.js
+++ server/src/controllers/contact.controller.js
@@ -1,9 +1,22 @@
-import { ApiError, asycHandler } from "../utils/index.js";
+import { ApiError, ApiResponse, asycHandler } from "../utils/index.js";
+import { sendEmail } from "../utils/nodemailer.utils.js";
 
 const contact = asycHandler(async (req, res) => {
 	try {
 		const { name, email, message } = req.body;
-		const userMsg = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
+		const emailsubject = "Portfolio Query";
+		const emailBody = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
+
+		const sendEmailRes = sendEmail(emailsubject, emailBody);
+
+		if (!sendEmail) {
+			return res
+				.status(411)
+				.json(new ApiResponse(411, false, "Email not send."));
+		}
+		return res
+			.status(200)
+			.json(new ApiResponse(200, true, "Email Send Successfully"));
 	} catch (error) {
 		throw new ApiError(
 			500,
---
+++ server/src/utils/nodemailer.utils.js
@@ -0,0 +1,23 @@
+import { createTransport } from "nodemailer";
+import { asycHandler } from "./asycHandler.util";
+
+const sendEmail = asycHandler(async (emailsubject, emailBody) => {
+	const transport = createTransport({
+		service: "gmail",
+		auth: {
+			user: process.env.APP_EMAIL,
+			pass: process.env.APP_PWD,
+		},
+	});
+
+	var mailOptions = {
+		from: process.env.APP_EMAIL,
+		to: process.env.APP_EMAIL,
+		subject: emailsubject,
+		text: emailBody,
+	};
+
+	return sendEmail;
+});
+
+export { sendEmail };
