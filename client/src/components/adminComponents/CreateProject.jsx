40201b134f3581e9891b31a16cadfa22ec42567a
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-04 21:22:55 +0530

    "Add form for project name input in CreateProject component"
    This commit adds a form with a single input field for the project name in the CreateProject component. The form includes a label for the input field and has some basic styling applied

--- client/src/components/adminComponents/CreateProject.jsx
+++ client/src/components/adminComponents/CreateProject.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function CreateProject() {
   return (
-    <div>CreateProject</div>
+    <div className='p-3 backdrop-blur-3xl rounded-md w-fit mx-auto mt-10'>
+      <h1 className='text-3xl lg:text-5xl text-white mb-5'>Create Project</h1>
+      <form className='flex flex-col gap-3'>
+        <input type="text" name="name" id="name" required placeholder='Project Name' className='bg-transparent px-3 py-2 border rounded-full w-full lg:w-[40vw] font-bold text-xl ' />
+      </form>
+    </div>
   )
 }
 
