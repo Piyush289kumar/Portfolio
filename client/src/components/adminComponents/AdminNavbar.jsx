433050f047895e1f6be4e478d94d0d61d6bc0255
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-04 20:59:54 +0530

    Update AdminNavbar.jsx: adjusts className attribute for responsive navigation behavior
    The changes include:
    
    * Modifying the `className` attribute of the `nav` element to improve responsiveness.
    * Adding a new

--- client/src/components/adminComponents/AdminNavbar.jsx
+++ client/src/components/adminComponents/AdminNavbar.jsx
@@ -8,9 +8,8 @@ function AdminNavbar() {
   const dispatch = useDispatch();
   return (
     <nav
-      className={`bg-white bg-opacity-5 backdrop-blur-xl text-white w-screen z-10 rounded-xl lg:rounded-none fixed lg:static h-screen lg:h-fit flex flex-col lg:flex-row justify-evenly lg:justify-between border border-none lg:border-gray-500 py-10 lg:py-3 lg:px-20 lg:translate-x-0 transition delay-150  ease-in-out ${
-        toggleNav ? "translate-x-0" : "-translate-x-full"
-      }`}
+      className={`bg-white bg-opacity-5 backdrop-blur-xl text-white w-screen z-10 rounded-xl lg:rounded-none fixed lg:static h-screen lg:h-fit flex flex-col lg:flex-row justify-evenly lg:justify-between border border-none lg:border-gray-500 py-10 lg:py-3 lg:px-20 lg:translate-x-0 transition delay-150  ease-in-out ${toggleNav ? "translate-x-0" : "-translate-x-full"
+        }`}
     >
       <AiOutlineClose
         className={`absolute top-5 right-5 text-lg hover:scale-125 cursor-pointer hover:text-red-400 transition-all lg:hidden`}
@@ -40,6 +39,7 @@ function AdminNavbar() {
             className="text-center lg:text-start cursor-pointer hover:bg-purple-500 hover:bg-opacity-60 hover:shadow-lg px-2 py-1 transition-all outline-none rounded-2xl"
             onClick={() => {
               dispatch(setPage(tab.link));
+              dispatch(toggle())
             }}
           >
             {tab.name}
