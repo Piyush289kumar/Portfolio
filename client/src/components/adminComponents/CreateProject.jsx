0a3e9ec632065a025a652b96c8b61f23fb2850ed
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-04 21:47:24 +0530

    "Added state and handler for file input in CreateProject component"
    This commit updates the CreateProject component to include state for storing the project image and handles the image file input. It also updates the input fields to use the state and handles

--- client/src/components/adminComponents/CreateProject.jsx
+++ client/src/components/adminComponents/CreateProject.jsx
@@ -1,27 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function CreateProject() {
-  return (
-    <div className='p-3 backdrop-blur-3xl rounded-md w-fit mx-auto mt-10'>
-      <h1 className='text-3xl lg:text-5xl text-white mb-7 px-3'>Create Project</h1>
-      <form className='flex flex-col gap-3'>
-        <input type="text" name="name" id="name" required placeholder='Project Name' className='bg-transparent px-3 py-2 border rounded-full w-full lg:w-[40vw] font-bold text-xl gradientText' />
+  const [projectName, setprojectName] = useState("");
+  const [projectDes, setProjectDes] = useState("");
+  const [projectImg, setProjectImg] = useState("");
+  const [githubLink, setGithubLink] = useState("");
+  const [hostedLink, setHostedLink] = useState("");
 
-        <textarea name="des" rows='5' id="des" placeholder='Project Description' className='bg-transparent px-3 py-2 border rounded-3xl w-full lg:w-[40vw] font-bold text-xl gradientText' ></textarea>
+  const handleImage = (receivedImgEvent) => {
+    const imgFile = receivedImgEvent.target.files[0]
+    setProjectImg(imgFile)
+    console.log(imgFile);
+  }
+
+
+  return (
+    <div className="p-3 backdrop-blur-3xl rounded-md w-fit mx-auto mt-10">
+      <h1 className="text-3xl lg:text-5xl text-white mb-7 px-3">
+        Create Project
+      </h1>
+      <form className="flex flex-col gap-3">
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          value={projectName}
+          onChange={(event) => setprojectName(event.target.value)}
+          placeholder="Project Name"
+          className="bg-transparent px-3 py-2 border rounded-full w-full lg:w-[40vw] font-bold text-xl gradientText"
+        />
+
+        <textarea
+          name="des"
+          rows="5"
+          id="des"
+          value={projectDes}
+          onChange={(event) => setProjectDes(event.target.value)}
+          placeholder="Project Description"
+          className="bg-transparent px-3 py-2 border rounded-3xl w-full lg:w-[40vw] font-bold text-xl gradientText"
+        ></textarea>
 
         <div>
-          <label htmlFor="img">Project Image: </label>
-          <input type="file" name="img" id="img" />
+          <label
+            htmlFor="img"
+            className="bg-transparent px-3 py-2 font-bold text-xl gradientText"
+          >
+            Project Image:
+          </label>
+          <input type="file" name="img" id="img" accept="image" required onChange={handleImage} />
         </div>
 
-        <input type="text" name="githubLink" id="name" required placeholder='Github Link' className='bg-transparent px-3 py-2 border rounded-full w-full lg:w-[40vw] font-bold text-xl gradientText' />
+        <input
+          type="text"
+          name="githubLink"
+          id="name"
+          required
+          value={githubLink}
+          onChange={(event) => setGithubLink(event.target.value)}
+          placeholder="Github Link"
+          className="bg-transparent px-3 py-2 border rounded-full w-full lg:w-[40vw] font-bold text-xl gradientText"
+        />
 
-        <input type="text" name="hostedLink" id="name" required placeholder='Hosted Link' className='bg-transparent px-3 py-2 border rounded-full w-full lg:w-[40vw] font-bold text-xl gradientText' />
+        <input
+          type="text"
+          name="hostedLink"
+          id="name"
+          required
+          value={hostedLink}
+          onChange={(event) => setHostedLink(event.target.value)}
+          placeholder="Hosted Link"
+          className="bg-transparent px-3 py-2 border rounded-full w-full lg:w-[40vw] font-bold text-xl gradientText"
+        />
 
-        <button type="submit" className='bg-purple-500 px-3 py-2 border rounded-full mx-auto w-64 font-bold text-xl text-white'>Create Project</button>
+        <button
+          type="submit"
+          className="bg-purple-500 px-3 py-2 border rounded-full mx-auto w-64 font-bold text-xl text-white"
+        >
+          Create Project
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
-export default CreateProject
+export default CreateProject;
