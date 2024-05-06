37ed242784382e12e69c937f52846959b16dd1dd
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-06 11:51:28 +0530

    Stuck on Geting All Project Component

--- client/src/components/adminComponents/AllProject.jsx
+++ client/src/components/adminComponents/AllProject.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function AllProject() {
-  return (
-    <div>AllProject</div>
-  )
-}
+  
+  const [AllProject, setAllProject] = useState([]);
 
-export default AllProject
+  useEffect(() => {
+    const getAllProject = async () => {
+      const response = await axios.get(
+        "http://localhost:5001/api/v1/get-project/"
+      );
+      const allProjectRes = response.data.data;
+
+      console.log("allProjectRes");
+      console.log(allProjectRes);
+      setAllProject(allProjectRes);
+
+      console.log(AllProject);
+    };
+    getAllProject();
+  }, []);
+
+  return (
+    <div>
+      {AllProject.map((project, idx) => (
+        <h1 key={idx}>{project.name}</h1>
+      ))}
+    </div>
+  );
+}
+export default AllProject;
--- client/src/components/adminComponents/AllSkill.jsx
+++ client/src/components/adminComponents/AllSkill.jsx
@@ -1,29 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 function AllSkill() {
-  const [AllSkill, setAllSkill] = useState([
-    {
-      description: "pdesnnnn",
-      githubLink: "gitnm",
-      hostedUrl: "Hosted Link",
-      img: "https://res.cloudinary.com/des1uatms/image/upload/v1714929856/porfolio/en1ixdxnd7ylgweszbjy.jpg",
-      name: "pnamenn",
-    },
-  ]);
+
+  const [AllSkill, setAllSkill] = useState([]);
+
   useEffect(() => {
     const getAllSkills = async () => {
       const response = await axios.get(
-        "http://localhost:5001/api/v1/get-project/"
+        "http://localhost:5001/api/v1/get-skill/"
       );
       const allSkillResponse = response.data.data;
       setAllSkill(allSkillResponse);
     };
+    getAllSkills();
   }, []);
+  
   return (
     <div>
       {AllSkill.map((skill, idx) => (
         <h1 className="text-5xl text-white m-5" key={idx}>
-          {skill.name}
+          {skill.skillName}
         </h1>
       ))}
     </div>
