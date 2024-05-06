e50ec9b93a9edf9bc9d15737ab0366834c0784e9
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-06 11:22:10 +0530

    "Convert class component to functional component and fetch data from API"
    This commit converts the `AllSkill` component from a class component to a functional component. It also uses the `useState` and `useEffect` hooks to

--- client/src/components/adminComponents/AllSkill.jsx
+++ client/src/components/adminComponents/AllSkill.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function AllSkill() {
+  const [AllSkill, setAllSkill] = useState([
+    {
+      description: "pdesnnnn",
+      githubLink: "gitnm",
+      hostedUrl: "Hosted Link",
+      img: "https://res.cloudinary.com/des1uatms/image/upload/v1714929856/porfolio/en1ixdxnd7ylgweszbjy.jpg",
+      name: "pnamenn",
+    },
+  ]);
+  useEffect(() => {
+    const getAllSkills = async () => {
+      const response = await axios.get(
+        "http://localhost:5001/api/v1/get-project/"
+      );
+      const allSkillResponse = response.data.data;
+      setAllSkill(allSkillResponse);
+    };
+  }, []);
   return (
-    <div>AllSkill</div>
-  )
+    <div>
+      {AllSkill.map((skill, idx) => (
+        <h1 className="text-5xl text-white m-5" key={idx}>
+          {skill.name}
+        </h1>
+      ))}
+    </div>
+  );
 }
-
-export default AllSkill
+export default AllSkill;
