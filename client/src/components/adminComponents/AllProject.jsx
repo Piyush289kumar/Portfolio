059761d15356013a6ba6dc5703575bca3917f8af
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-07 16:43:46 +0530

    "Updated AllProject component to remove unnecessary state variables and functions"
    Explanation:
    
    The updated code for the AllProject component has removed several unnecessary state variables and functions. Specifically, the state variables `selectedProject`, `update

--- client/src/components/adminComponents/AllProject.jsx
+++ client/src/components/adminComponents/AllProject.jsx
@@ -7,20 +7,16 @@ import { TiTick } from "react-icons/ti";
 import { getSignature } from "../../utils/getSignature.utils.js";
 import { deleteImage } from "../../utils/deleteImage.utils.js";
 import { uploadOnCloudinary } from "../../utils/cloudinary.utils.js";
-
 function AllProject() {
   const [edit, setEdit] = useState(false);
   const [selectedProject, setSelectedProject] = useState("");
-
   const [updateProjectName, setUpdateProjectName] = useState("");
   const [updateProjectDes, setUpdateProjectDes] = useState("");
   const [updateProjectGithubLink, setUpdateProjectGithubLink] = useState("");
   const [updateProjectHostedLink, setUpdateProjectHostedLink] = useState("");
   const [updatedProjectImg, setUpdatedProjectImg] = useState("");
-
   const allProject = useSelector((state) => state.showcase.project);
   const dispatch = useDispatch();
-
   const getAllProject = async () => {
     const response = await axios.get(
       "http://localhost:5001/api/v1/get-project/"
@@ -30,16 +26,12 @@ function AllProject() {
     console.log(resData);
     dispatch(setProject(resData));
   };
-
   const deleteProjectHandler = async (id) => {
     alert("Are you sure you want to delete this project?");
-
     // const signature = await getSignature(publicId);
     // console.log(`Signature generate while Deleting : ${signature}`);
-
     // // Delete Image from cloudinary
     // const newData = await deleteImage(publicId, signature);
-
     // Delete Project from database
     const deleteProject = await axios.delete(
       `http://localhost:5001/api/v1/delete-project/${id}`
@@ -48,19 +40,16 @@ function AllProject() {
     alert(dataRes.message);
     getAllProject();
   };
-
   const handleImg = (e) => {
     const file = e.target.files[0];
     setUpdatedProjectImg(file);
   };
   const updateProjectHandler = async (id) => {
     setEdit(false);
-
     let uploadImg;
     if (updatedProjectImg) {
       uploadImg = await uploadOnCloudinary(updatedProjectImg);
     }
-
     const updatedData = {
       id: id,
       name: updateProjectName,
@@ -71,21 +60,17 @@ function AllProject() {
       deleteToken: uploadImg ? uploadImg.deleteToken : null,
       img: uploadImg ? uploadImg.secureUrl : null,
     };
-
     const res = await axios.put(
       `http://localhost:5001/api/v1/update-project/${id}`,
       updatedData
     );
     const data = await res.data;
     alert(data.message);
-
     getAllProject();
   };
-
   useEffect(() => {
     getAllProject();
   }, []);
-
   return (
     <div className="flex flex-col justify-center items-center gap-5 text-white lg:h-auto">
       {allProject.map((project, idx) => (
@@ -109,7 +94,6 @@ function AllProject() {
               onChange={handleImg}
               className={`${edit ? "block" : "hidden"}`}
             />
-
             <div className="flex flex-col gap-2">
               <h1
                 className={`text-xl lg:text-3xl ${
@@ -137,7 +121,6 @@ function AllProject() {
                   project.name
                 )}
               </h1>
-
               <div>
                 <label htmlFor="des">Desc: </label>
                 <h1
@@ -167,7 +150,6 @@ function AllProject() {
                   )}
                 </h1>
               </div>
-
               <div>
                 <label htmlFor="githubLink">Github Link: </label>
                 <h1
@@ -197,10 +179,8 @@ function AllProject() {
                   )}
                 </h1>
               </div>
-
               <div>
                 <label htmlFor="hostedLink">Hosted Link: </label>
-
                 <h1
                   className={`text-sm lg:text-base text-gray-300 ${
                     edit &&
