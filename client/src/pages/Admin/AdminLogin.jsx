d4d598c372ef6b2f5ef94b3e1e8162eed191e3aa
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-09 20:28:53 +0530

    Login Page Build

--- client/src/pages/Admin/AdminLogin.jsx
+++ client/src/pages/Admin/AdminLogin.jsx
@@ -1,9 +1,34 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, setLoginUserEmail } from "../../redux/Slice/authSlice";
 function AdminLogin() {
+  const useLoggIn = useSelector((state) => state.auth.isLoggin);
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const loginHandler = async (event) => {
+    event.preventDefault();
+    const res = await axios.post(`${import.meta.env.VITE_SERVER_URL}/signin`, {
+      email,
+      password: pwd,
+    });
+    if (res.data.success) {
+      dispatch(login());
+      dispatch(setLoginUserEmail(email));
+      setEmail("");
+      setPwd("");
+    } else {
+      dispatch(logout());
+    }
+    alert(res.data.message);
+  };
   return (
     <div className="flex justify-center mx-auto py-32 lg:pt-36">
-      <form className="flex flex-col bg-transparent backdrop-blur-3xl w-[45vw] lg:w-1/3 border border-zinc-300 rounded-3xl px-12 py-14 text-xl lg:text-2xl shadow-2xl">
+      <form
+        onSubmit={loginHandler}
+        className="flex flex-col bg-transparent backdrop-blur-3xl w-[45vw] lg:w-1/3 border border-zinc-300 rounded-3xl px-12 py-14 text-xl lg:text-2xl shadow-2xl"
+      >
         <h1 className="gradientText fw-bolder uppercase text-center pb-10">
           Login
         </h1>
@@ -12,6 +37,8 @@ function AdminLogin() {
           name="email"
           id="email"
           placeholder="Enter Your Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
           className="bg-transparent gradientText border border-zinc-300 rounded-full px-5 py-2 focus:border focus:border-zinc-300 mb-8"
         />
         <input
@@ -19,6 +46,8 @@ function AdminLogin() {
           name="password"
           id="password"
           placeholder="Password"
+          value={pwd}
+          onChange={(event) => setPwd(event.target.value)}
           className="bg-transparent gradientText border border-zinc-300 rounded-full px-5 py-2 focus:border focus:border-zinc-300"
         />
         <button
@@ -31,5 +60,4 @@ function AdminLogin() {
     </div>
   );
 }
-
 export default AdminLogin;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import navSlice from "./Slice/navSlice.js";
 import pageSlice from "./Slice/pageSlice.js";
 import showCaseSlice from "./Slice/showCaseSlice.js";
+import authSlice from "./Slice/authSlice.js";
 
 const store = configureStore({
   reducer: {
     nav: navSlice,
     page: pageSlice,
     showcase: showCaseSlice,
+    auth: authSlice,
   },
 });
 
