b5c3c330f70bd25324f3960751776c98958ddc12
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-10 20:53:14 +0530

    Update ProtectedRoutes component
    * Remove unnecessary code
    * Update PropTypes syntax to ES6 syntax

--- client/src/components/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes.jsx
@@ -5,9 +5,7 @@ const ProtectedRoutes = ({ element }) => {
   const auth = useSelector((state) => state.auth.isLoggin);
   return !auth ? <Navigate to={"/login"} /> : element;
 };
-
 ProtectedRoutes.prototype = {
   element: PropTpes.element.isRequired,
-}; 
-
+};
 export default ProtectedRoutes;
