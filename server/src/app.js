7a834441212de4ffad5b4cd725bd19fac7810f7e
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-11 21:27:13 +0530

    "Updated server/src/app.js: removed unnecessary imports, updated file limit, and modified CORS options"

--- server/src/app.js
+++ server/src/app.js
@@ -2,26 +2,18 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
-
 const app = express();
 const fileLimit = "16kb";
-
 app.use(express.json({ limit: fileLimit }));
 app.use(express.urlencoded({ extended: true, limit: fileLimit }));
 app.use(express.static("public"));
-
 const corsOptions = {
 	origin: "http://localhost:5173",
 };
-
 app.use(cors(corsOptions));
-
 // Import Route
 import router from "./routes/user.route.js";
-
 // Roter Declearation
 app.use("/api/v1/", router);
-
 app.use(cookieParser());
-
 export { app };
