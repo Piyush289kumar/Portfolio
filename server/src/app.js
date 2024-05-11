01456d970154819fbcad693cdcb28ddb4365dc6e
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-11 21:27:53 +0530

    "Updated server/src/app.js: added jsonwebtoken, increased file limit, and imported user route"
    This commit updates the file server/src/app.js by adding jsonwebtoken, increasing the file limit

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
+
 const app = express();
 const fileLimit = "16kb";
 app.use(express.json({ limit: fileLimit }));
@@ -11,8 +12,10 @@ const corsOptions = {
 	origin: "http://localhost:5173",
 };
 app.use(cors(corsOptions));
+
 // Import Route
 import router from "./routes/user.route.js";
+
 // Roter Declearation
 app.use("/api/v1/", router);
 app.use(cookieParser());
