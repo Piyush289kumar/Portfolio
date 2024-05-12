4b8854d3fe267b596bcc99b63783f4d26135a28f
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-12 20:44:06 +0530

    Logout Func

--- client/src/components/adminComponents/AdminNavbar.jsx
+++ client/src/components/adminComponents/AdminNavbar.jsx
@@ -4,20 +4,28 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggle } from "../../redux/Slice/navSlice";
 import { setPage } from "../../redux/Slice/pageSlice";
 import axios from "axios";
+import { logout } from "../../redux/Slice/authSlice";
 function AdminNavbar() {
   const toggleNav = useSelector((state) => state.nav.toggleNav);
   const dispatch = useDispatch();
 
-
-  const logoutHandler = async ()=>{
+  const logoutHandler = async () => {
     const response = await axios.get(
-      `${import.meta.env.VITE_SERVER_URL}/get-project`
+      `${import.meta.env.VITE_SERVER_URL}/logout`
     );
-  }
+    const resData = await response.data.data;
+    const data = await resData.data;
+
+    if (data.success) {
+      dispatch(logout());
+      navigate("/");
+    }
+  };
   return (
     <nav
-      className={`bg-white bg-opacity-5 backdrop-blur-xl text-white w-screen z-10 rounded-xl lg:rounded-none fixed lg:static h-screen lg:h-fit flex flex-col lg:flex-row justify-evenly lg:justify-between border border-none lg:border-gray-500 py-10 lg:py-3 lg:px-20 lg:translate-x-0 transition delay-150  ease-in-out ${toggleNav ? "translate-x-0" : "-translate-x-full"
-        }`}
+      className={`bg-white bg-opacity-5 backdrop-blur-xl text-white w-screen z-10 rounded-xl lg:rounded-none fixed lg:static h-screen lg:h-fit flex flex-col lg:flex-row justify-evenly lg:justify-between border border-none lg:border-gray-500 py-10 lg:py-3 lg:px-20 lg:translate-x-0 transition delay-150  ease-in-out ${
+        toggleNav ? "translate-x-0" : "-translate-x-full"
+      }`}
     >
       <AiOutlineClose
         className={`absolute top-5 right-5 text-lg hover:scale-125 cursor-pointer hover:text-red-400 transition-all lg:hidden`}
@@ -47,14 +55,17 @@ function AdminNavbar() {
             className="text-center lg:text-start cursor-pointer hover:bg-purple-500 hover:bg-opacity-60 hover:shadow-lg px-2 py-1 transition-all outline-none rounded-2xl"
             onClick={() => {
               dispatch(setPage(tab.link));
-              dispatch(toggle())
+              dispatch(toggle());
             }}
           >
             {tab.name}
           </li>
         ))}
       </ul>
-      <button className="text-2xl px-2 py-1 text-center hover:bg-red-600 hover:bg-opacity-60 hover:shadow-lg rounded-2xl transition-all outline-none" onClick={logoutHandler}>
+      <button
+        className="text-2xl px-2 py-1 text-center hover:bg-red-600 hover:bg-opacity-60 hover:shadow-lg rounded-2xl transition-all outline-none"
+        onClick={logoutHandler}
+      >
         Logout
       </button>
     </nav>
